8537d9de4438a7ab4f8946f6ded3946098adbbad
James-Wolfley <73006624+James-Wolfley@users.noreply.github.com>
2024-05-06 17:38:29 -0400

    Fixed Lab 2
    Added back accidentally removed code to main.cpp

--- Lab 2  CMPSC 121/CMPSC Lab 2/main.cpp
+++ Lab 2  CMPSC 121/CMPSC Lab 2/main.cpp
@@ -1,17 +1,48 @@
-//Author    :   James P Wolfley
-//Class     :   CMPSC 121
-//Purpose   :	
-//Sources of logic assistance: None
-
 #include <iostream>
-#include <iomanip>
-#include <fstream>
 
 using namespace std;
 
-ofstream fout;
-
 int main()
 {
+	int sum, num1, num2, num3, num4;
+
+	cout << "Please enter a 4 digit integer." << endl;
+
+	cin >> sum;
+
+	num4 = sum % 10;
+	num3 = sum / 10 % 10;
+	num2 = sum / 100 % 10;
+	num1 = sum / 1000;
+
+	sum = num1 + num2 + num3 + num4;
+
+	cout << "Sum of the digits: " << num1 << " + " << num2 << " + " << num3 << " + " << num4 << " = " << sum << endl;
+
+	num4 = sum % 10;
+	num3 = sum / 10 % 10;
+	num2 = sum / 100 % 10;
+	num1 = sum / 1000;
+
+	sum = num1 + num2 + num3 + num4;
+
+	cout << "Sum of the digits of the sum: " << num1 << " + " << num2 << " + " << num3 << " + " << num4 << " = " << sum << endl;
+
+	num4 = sum % 10;
+	num3 = sum / 10 % 10;
+	num2 = sum / 100 % 10;
+	num1 = sum / 1000;
+
+	sum = num1 + num2 + num3 + num4;
+
+	cout << "Sum of the digits of the sum of the digits of the sum: " << num1 << " + " << num2 << " + " << num3 << " + " << num4 << " = " << sum << endl;
+
 }
 
+/*
+Please enter a 4 digit integer.
+1423
+Sum of the digits: 1 + 4 + 2 + 3 = 10
+Sum of the digits of the sum: 0 + 0 + 1 + 0 = 1
+Sum of the digits of the sum of the digits of the sum: 0 + 0 + 0 + 1 = 1
+*/
